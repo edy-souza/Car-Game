a51b30bb2ddfd384c4173fc6046ddf61864bdd2b
edy-souza <edysouza363@gmail.com>
2023-08-18 22:38:11 -0300

    Created Version

---
+++ ator.js
@@ -0,0 +1,17 @@
+// variaveis do ator
+let xAtor = 100;
+let yAtor = 366;
+
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, 30, 30);
+}
+
+function movimentaAtor(){
+  if(keyIsDown(UP_ARROW)){
+    yAtor -= 5;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    yAtor += 5;
+  }
+}
